91da8d6f2dd48aaf5867601d443e5b4e313a57c6
Antonio Salmeron <mr.tonyboi@gmail.com>
2021-11-12 13:10:46 -0800

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+# Mod 0 Assessment
+
+animal = "panda"
+is_cuddly = false
+
+animal = "tiger"
+
+num_8 = 8.0
+num_3 = 3.0
+
+quotient = num_3 / num_8
+
+favorite_films = ["Step Brothers", "Pineapple Express", "Superbad"]
+
+favorite_films.shift()
+
+puts "The number of characters in #{animal} is #{animal.length}"
