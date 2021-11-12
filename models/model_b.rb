02fbdb60d7e753f87337a399a2b88ebb7c5f00b0
Antonio Salmeron <mr.tonyboi@gmail.com>
2021-11-12 13:20:56 -0800

    Initial commit

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+house_model = {
+  location: "California",
+  design: "Modern",
+  zip_code: 99999,
+  bedrooms: 7,
+  safe_area: true,
+  good_investment: false,
+  key_attributes: ["Big backyard", "Pool", "Guest house"],
+  school_districts: ["OUSD", "AUHSD"]
+
+}
