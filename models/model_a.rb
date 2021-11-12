5f8f68d88cbc2fff2615e46941737be7f3d44000
Antonio Salmeron <mr.tonyboi@gmail.com>
2021-11-12 13:16:33 -0800

    Initial commit

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+laptop_model = {
+  brand: "Apple",
+  model: "MacBook Prop",
+  quantity: 1,
+  year: 2021,
+  reliable: true,
+  effective: true,
+  purpose: ["Research", "Code", "Create"],
+  owner: ["Antonio"]
+
+}
